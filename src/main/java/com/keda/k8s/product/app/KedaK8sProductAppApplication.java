82e4edfdd9d2ecf5b65362f78ae9d93b590513cf
manivannancsmca <manivannancsmca@gmail.com>
2026-07-29 13:50:02 +0530

    first commit

---
+++ src/main/java/com/keda/k8s/product/app/KedaK8sProductAppApplication.java
@@ -0,0 +1,13 @@
+package com.keda.k8s.product.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedaK8sProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KedaK8sProductAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keda/k8s/product/app/KedaK8sProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keda.k8s.product.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedaK8sProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
