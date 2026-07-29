0807f7290c0fe25b42d13c4811b5b62322f3d4f6
manivannancsmca <manivannancsmca@gmail.com>
2026-07-29 14:14:10 +0530

    ProductResponse pojo class added

--- src/main/java/com/keda/k8s/product/app/dto/ProductResponse.java
+++ src/main/java/com/keda/k8s/product/app/dto/ProductResponse.java
@@ -1,5 +1,23 @@
 package com.keda.k8s.product.app.dto;
 
-public class ProductResponse {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import com.keda.k8s.product.app.entity.ProductCategory;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private ProductCategory category;
+    private String imageUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
